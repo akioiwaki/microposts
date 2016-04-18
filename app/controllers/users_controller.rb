7b985fab00cd066fa445e360fdcde3b3c7f3ab9b
akioiwaki <akioiwaki@gmail.com>
2016-04-18 15:21:32 +0000

    user-profile-2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show, :edit, :update]
   def show 
    @user = User.find(params[:id])
   end
@@ -18,27 +19,24 @@ class UsersController < ApplicationController
   end
   
   def edit
-    # @user = User.find(params[:id])
-    # if @user.save
-    #   flash[:success] = "プロフィールが変更されました"
-    #   redirect_to @user
-    # else
-    #   render 'edit'
-    # end
+    @user = User.find(params[:id])
   end
   
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
+    if @user.update(user_params)
+      redirect_to @user
     else 
       render 'edit'
     end
   end
-    
-    def destroy
-      session[:user_id] = nil
-      redirect_to root_path
+  
+  def logged_in_user
+    unless logged_in?
+    flash[:danger] ="Please log in"
+    redirect_to login_url
     end
+  end
     
   private
 
